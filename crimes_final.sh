3923a9089e98d25a39fe69a3718661c9999a8e64
Andrea Magana <andreamagana1@gmail.com>
2017-11-27 12:10:49 -0600

    date, primary_type, community area 2011-2015

--- crimes_final.sh
+++ crimes_final.sh
@@ -1,4 +1,4 @@
-#variable list for reference 
+#variable list for reference
 ID,
 Case Number,
 Date,
@@ -27,3 +27,6 @@ cat Crimes_2001topresent.csv | cut -d, -f3,5-6,14,18 | grep -v "\b/2001\b"| grep
 
 #cut year to make file smaller
 cat Crimes_2001topresent.csv | cut -d, -f3,5-6,14 | grep -v "\b/2001\b"| grep -v "\b/2002\b"| grep -v "\b/2003\b" | grep -v "\b/2004\b" | grep -v "\b/2005\b" | grep -v "\b/2006\b" | grep -v "\b/2007\b" | grep -v "\b/2008\b" | grep -v "\b/2009\b" |grep -v "\b/2016\b" | grep -v "\b/2017\b" >> edited_crimes2.csv
+
+#saving only 2011 - 2012 crimes with limited columns - date, primary type, and community area
+cat Crimes_2001topresent.csv | cut -d, -f3,6,14 | grep -v "\b/2001\b"| grep -v "\b/2002\b"| grep -v "\b/2003\b" | grep -v "\b/2004\b" | grep -v "\b/2005\b" | grep -v "\b/2006\b" | grep -v "\b/2007\b" | grep -v "\b/2008\b" | grep -v "\b/2009\b" |grep -v "\b/2016\b" | grep -v "\b/2017\b" | grep -v "\b/2010\b" >> Chicrimes.csv
