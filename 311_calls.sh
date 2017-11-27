35bd0834fa46d8ecab42c930156dd35878d2f956
Andrea Magana <andreamagana1@gmail.com>
2017-11-27 12:18:28 -0600

    most up to date

--- 311_calls.sh
+++ 311_calls.sh
@@ -1,1 +1,1 @@
-cat calls311.csv | cut -d ',' -f1,3-6,8-10,16-18 | grep -v "\b/2008\b" | grep -v "\b/2009\b" |grep -v "\b/2016\b" | grep -v "\b/2017\b" >> edited_calls.csv
+cat calls311.csv | awk 'NF==4' | cut -d, -f3,20 | grep -v "\b/2008\b" | grep -v "\b/2009\b" |grep -v "\b/2016\b" | grep -v "\b/2017\b" | grep -v "\b/2010\b" >> edited_calls.csv
---
+++ acs.sh
@@ -0,0 +1,1 @@
+cat ACS_data.csv | awk 'NF==4'| cut -d -f3, X, X
